b598a5c7b2446b178b41c315c9376be667d2c048
Dave Ryan <dave@bluntworks.net>
2013-10-13 20:31:00 +0100

    no idea

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var log         = require('bitlog')
 var Users       = require('level-users')
 var tru         = require('through')
 var isEmail     = require('validator').validators.isEmail
+var bcrypt      = require('bcrypt')
+var randpass    = require('randpass')
 var sep = '\xff'
 var usr_prfx = sep + 'INDEX' + sep + 'username'
+
+var opts = { valueEncoding: 'json' }
 
 
 var Udb = function(db, o) {
@@ -16,7 +20,9 @@ var Udb = function(db, o) {
     ? ['username', 'email'].concat(o.indexes)
     : ['username', 'email']
 
+  //log('indexes', indexes, o)
   this.addIndexes(indexes, function(err) {
+    //log('addIndexes', arguments)
     if(err) return log('indexes created err', err)
   })
 }
@@ -43,22 +49,99 @@ Udb.prototype.check = function(req, cb) {
   self.get(qry, function(err, id) {
     if(err) return cb(err)
     self.auth(id, pass, function(err, user, put) {
-      log('auth', err, user, put)
+      //log('auth', err, user, put)
       if(err) return cb(err)
       if(!user) return cb(null, false)
       user.id = id
       cb(null, user)
     })
   })
-
 }
 
-module.exports = function(db, o) {
-  var db = ('string' == typeof db)
-  ? Udb(level(db), o)
-  : Udb(db, o)
+Udb.prototype.newUser = function(user, cb) {
+  var self = this
+  user.password = user.temp = randpass()
+  user.status = 'pending'
+  user.hash_id = hashit(user)
 
-  return db
+  this.create(user, function(err, id) {
+    if(err) return log('new user err ', err)
+    self.get(id, function(err, u) {
+      if(err) return cb(err)
+      user.id = id
+      cb(null, user)
+    })
+  })
+}
+
+Udb.prototype.getUser = function(hid, cb) {
+  var self = this
+  self.get(hid, function(err, id) {
+    if(err) return cb(err)
+    self.get(id, function(err, u) {
+      if(err) return cb(err)
+      u.id = id
+      cb(null, u)
+    })
+  })
+}
+
+Udb.prototype.confirm = function(hid, cb) {
+  var self = this
+  this.get({ hash_id: hid }, function(err, id) {
+    if(err) return cb(err)
+    self.get(id, function(err, u) {
+      if(err) return cb(err)
+      u.status = 'confirmed'
+      self.db.put(id, u, opts, function(err) {
+        log.warn('UDB UPDATED',hid, arguments)
+        cb(null, u)
+      })
+    })
+  })
+}
+
+Udb.prototype.password = function(o, cb) {
+  if(o.new_pass !== o.conf_pass) return cb('passwwords dont match')
+  var self = this
+  self.getUser({ hash_id: o.hash_id }, function(err, user) {
+    if(err) return cb(err)
+    if(user) {
+      bcrypt.hash(o.new_pass, 5, function(err, bcryptPass) {
+        log('bycrypt', err, bcryptPass, user)
+        if(err) return cb(err)
+        user.salt = bcryptPass
+        delete user.temp
+        self.db.put(self.prefix + user.id, user, function(err) {
+          log('update pass/user', err)
+          if(err) return cb(err)
+          cb(null, user)
+        })
+      })
+    }
+  })
+}
+
+// module.exports = function(db, o) {
+//   var db = ('string' == typeof db)
+//   ? Udb(level(db), o)
+//   : Udb(db, o)
+//
+//   return db
+// }
+
+module.exports = function(o) {
+  var db = { db: o.db }
+  delete o.db
+  return new Udb(db, o)
 }
 
 module.exports.routes = require('./routes.js')
+
+var crypto = require('crypto')
+function hashit(u) {
+  var sha = crypto.createHash('sha1')
+  u.date = new Date().getTime()
+  sha.update(JSON.stringify(u))
+  return sha.digest('hex')
+}
